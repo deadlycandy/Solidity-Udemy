bb9612218cbeca780a629dddb36a4364c45e7674
rabhatna <rabhatna@ucsc.edu>
2019-08-09 16:52:04 -0700

    Npm dependency and Mocha tests

---
+++ Inbox/test/Inbox.test.js
@@ -0,0 +1,33 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+
+const web3 = new Web3(ganache.provider()); //connects to the ethereum network.
+
+/* Mocha Test
+class Car{
+    park(){
+        return "stopped";
+    }
+
+    drive(){
+        return "vroom";
+    }
+}
+
+let car;
+
+beforeEach(() => { // used for common intialize code
+    car = new Car();
+});
+
+describe('Car',() => { // no link between the string and class name
+    it('Park function',() =>{
+        assert.equal(car.park(), 'stopped');
+    });
+
+    it('Drive function', () =>{
+        assert.equal(car.drive(), 'vroom');
+    });
+});
+*/
