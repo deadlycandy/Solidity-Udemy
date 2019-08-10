bb93024ae344fa4704f0f2ae155d8a1f00bf8f9f
rabhatna <rabhatna@ucsc.edu>
2019-08-09 18:29:55 -0700

    Passed mocha tests for set and get functions

--- Inbox/test/Inbox.test.js
+++ Inbox/test/Inbox.test.js
@@ -6,21 +6,30 @@ const web3 = new Web3(ganache.provider()); //connects to the ethereum network.
 const {interface, bytecode} = require('../compile');
 
 let accounts;
-let inbox;
+let inbox; // represenets contract living on the blockchain
 
 beforeEach( async () => {
     //get a list of all accounts
     accounts = await web3.eth.getAccounts(); //can connect to multiple cryptos.
 
     // use an accoount to deploy a contract
-    inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({data: bytecode, arguments: ['Hi there!']})
-        .send({from: accounts[0], gas: '1000000'});
+    inbox = await new web3.eth.Contract(JSON.parse(interface)) //Tells web3 about methods Inbox has
+        .deploy({data: bytecode, arguments: ['Hi there!']}) // Tells web3 to deploy a new copy of the contract.
+        .send({from: accounts[0], gas: '1000000'}); //Tells web3 to send out a transaction to create this contract.
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(inbox);
+        assert.ok(inbox.options.address); //checking if it is a defined value
+    });
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Hi there!');
+    });
+    it('can change the message', async () =>{
+        await inbox.methods.setMessage('bye').send({from: accounts[0]});
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'bye');
     });
 });
 
