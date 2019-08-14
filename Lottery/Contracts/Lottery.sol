c8969bf14d9a6a82eaf3dd6556819e31cbb59a36
rabhatna <rabhatna@ucsc.edu>
2019-08-13 21:38:00 -0700

    Adding randomness to lottery contract

--- Lottery/Contracts/Lottery.sol
+++ Lottery/Contracts/Lottery.sol
@@ -9,6 +9,11 @@ contract Lottery {
     }
 
     function enter() public payable{
+        require(msg.value > .01 ether);
         players.push(msg.sender);
+    }
+
+    function random() private view returns(uint) {
+        sha3(block.difficulty, now, players);
     }
 }
