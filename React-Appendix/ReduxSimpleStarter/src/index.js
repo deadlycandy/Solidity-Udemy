ec8e6d391270965ed667ffb81c4a3bf032b4af3c
rabhatna <rabhatna@ucsc.edu>
2019-09-03 15:59:01 -0700

    Adding element to dom using react

--- React-Appendix/ReduxSimpleStarter/src/index.js
+++ React-Appendix/ReduxSimpleStarter/src/index.js
@@ -1,7 +1,11 @@
+// import React from 'react';
+const React = require('react');
+const ReactDOM = require('react-dom');
 // Creating new component. Produce some html.
-const App = function()
-{
-    return <div> Hi There </div>;
+const App = () =>{
+    return <div> Hi! </div>;
 }
 
 //Insert into html document
+// Rendering to the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
